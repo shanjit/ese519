5ce86828969168f54660e3d5ecc606f5817f13a9
Shanjit Singh Jajmann <shanjitsingh@gmail.com>
2015-02-08 17:50:32 -0500

    Final

--- lab1/Part1/mbed_07022015.cpp
+++ lab1/Part1/mbed_07022015.cpp
@@ -1,14 +1,26 @@
+/*
+ese519-lab1-part1
+    Vaibhav N. Bhat (vaibhavn@seas)
+    Shanjit S. Jajmann (sjajmann@seas)
+
+Idea : Using the keypad, poll the hash key / 2 for active high 
+
+*/
 #include "mbed.h"
 
+// define the leds - visual feedback
 DigitalOut myled1(LED1);
 DigitalOut myled2(LED2);
 DigitalOut myled3(LED3);
 DigitalOut myled4(LED4);
+
+// define rows for the keypad
 DigitalOut row1(p9);
 DigitalOut row2(p10);
 DigitalOut row3(p11);
 DigitalOut row4(p12);
 
+// define the column for the keypad
 DigitalIn col4(p5);
 DigitalIn col3(p6);
 DigitalIn col2(p7);
@@ -16,6 +28,7 @@ DigitalIn col1(p8);
 
 int main() {
 
+    // startup pattern to verify that code did load onto the board
     myled1 = 1;
     myled2 = 1;
     myled3 = 1;
@@ -28,6 +41,7 @@ int main() {
     myled3 = 0;
     myled4 = 0;
 
+    // initialize rows of the keypad
     row1 = 0;
     row2 = 0;
     row3 = 0;
@@ -35,7 +49,7 @@ int main() {
 
     while(1) {
 
-        // switch on row 1 and check all columns sequentially
+        // switch on row 1 and check column 
         row2 = 1;
         row1 = 0;
         row3 = 0;
@@ -49,7 +63,7 @@ int main() {
         wait(0.2);    
         }
 
-        // switch on row 1 and check all columns sequentially
+        // switch on row 3 and check column 
         row1 = 0;
         row2 = 0;
         row3 = 1;
