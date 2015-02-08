cbab861545d08819fb12f2df9c34b3e734ff4bf1
Shanjit Singh Jajmann <shanjitsingh@gmail.com>
2015-02-08 18:00:15 -0500

    Update mbed_07022015.cpp

--- lab1/Part1/mbed_07022015.cpp
+++ lab1/Part1/mbed_07022015.cpp
@@ -8,6 +8,15 @@ Idea : Using the keypad, poll the hash key / 2 for active high
 */
 #include "mbed.h"
 
+// Connections on the mbed
+// Mbed - Keypad 
+// p5 - 7
+// p6 - 6 
+// p7 - 5
+// p8 - 3
+// p10 - 8
+// p11 - 4
+
 // define the leds - visual feedback
 DigitalOut myled1(LED1);
 DigitalOut myled2(LED2);
@@ -49,13 +58,21 @@ int main() {
 
     while(1) {
 
+// Connections on the mbed
+// Mbed - Keypad 
+// p5 - 7
+// p6 - 6 
+// p7 - 5
+// p8 - 3
+// p10 - 8
+// p11 - 4
         // switch on row 1 and check column 
         row2 = 1;
         row1 = 0;
         row3 = 0;
         row4 = 0;
         
-        if(col2 == 1)
+        if(col2 == 1) // col3 = p6
         {
         myled1 = 1;
         wait(0.2);
@@ -69,7 +86,7 @@ int main() {
         row3 = 1;
         row4 = 0;
 
-        if(col3 == 1)
+        if(col3 == 1) // col2 - p7
         {
         myled2 = 1;
         wait(0.2);
